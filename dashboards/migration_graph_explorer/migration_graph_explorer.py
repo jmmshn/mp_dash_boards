7481d0b6350d4195f32469426298460823c54b39
jmmshn <jmmshn@gmail.com>
2020-09-24 00:41:52 -0700

    smarter searching, code cleanup

--- dashboards/migration_graph_explorer/migration_graph_explorer.py
+++ dashboards/migration_graph_explorer/migration_graph_explorer.py
@@ -1,4 +1,3 @@
-# %%
 from collections import defaultdict
 from typing import List, Union
 from monty.serialization import loadfn
@@ -38,7 +37,6 @@ with descriptor_db as db:
 
 all_ids_options = [{"label": _, "value": _} for _ in all_ids]
 
-# %%
 # recursive defualtdict
 
 
@@ -46,45 +44,66 @@ def rec_dd():
     return defaultdict(rec_dd)
 
 
-res_dict = rec_dd()
-with descriptor_db as db:
-    for cc in db.query():
-        try:
-            condensed_structure = cc["migration_graph"]["condensed_structure"]
-        except Exception:
-            continue
-        if not condensed_structure:
-            continue
-
-        dim = condensed_structure["dimensionality"]
-        sys = condensed_structure["crystal_system"]
-        m_type = condensed_structure["mineral"]["type"]
-        res_dict[f"D-{dim}"][sys][m_type][cc["snl_id"]] = 1
-sunburst_data = []
-
-level_data = {}
+tree_like_data = rec_dd()  # in the form of layer
 
 
-def recurse_fill(d, full_id=""):
-    if isinstance(d, int):
-        a, b, c, d = full_id.split(".")
-        level_data[d] = ".".join([a, b, c])
-        return 1
-    tot = 0
-    for k, v in d.items():
-        if len(full_id) == 0:
-            new_key = f"{k}"
-        else:
-            new_key = f"{full_id}.{k}"
-        sub_tot = recurse_fill(v, full_id=new_key)
-        sunburst_data.append(
-            {"id": new_key, "parent": full_id, "val": sub_tot})
-        tot += sub_tot
-    return tot
+def get_sunburst_data():
+    """
+    Read that data in the descriptor_db into a nested dictionary.
+    Then parse that dictionary into a set of parent-child relationships
+    for the sunburst plots
+
+    Returns:
+        List[dict]: A list of where each entry is
+            {
+                "id" : <<reference to current node>>>,
+                "parent" : <<reference to the parent>>,
+                "val": <<subtotal counter for that branch of the tree>>
+            }
+    """
+
+    sunburst_data = []
+
+    with descriptor_db as db:
+        for cc in db.query():
+            try:
+                condensed_structure = cc["migration_graph"]["condensed_structure"]
+            except Exception:
+                continue
+            if not condensed_structure:
+                continue
+
+            dim = condensed_structure["dimensionality"]
+            sys = condensed_structure["crystal_system"]
+            m_type = condensed_structure["mineral"]["type"]
+            tree_like_data[f"D-{dim}"][sys][m_type][cc["snl_id"]] = 1
+
+    level_data = {}
+
+    def recurse_fill(d, full_id=""):
+        if isinstance(d, int):
+            a, b, c, d = full_id.split(".")
+            level_data[d] = ".".join([a, b, c])
+            return 1
+        tot = 0
+        for k, v in d.items():
+            if len(full_id) == 0:
+                new_key = f"{k}"
+            else:
+                new_key = f"{full_id}.{k}"
+            sub_tot = recurse_fill(v, full_id=new_key)
+            sunburst_data.append(
+                {"id": new_key, "parent": full_id, "val": sub_tot})
+            tot += sub_tot
+        return tot
+
+    # Run the
+    recurse_fill(tree_like_data)
+    return sunburst_data, level_data
 
 
-# Run the
-recurse_fill(res_dict)
+# get the data needed to generate the sunburst plot
+sunburst_data, level_data = get_sunburst_data()
 
 sb_ids = []
 sb_labs = []
@@ -99,7 +118,6 @@ for cc in sunburst_data:
     sb_values.append(cc["val"])
 
 
-# %%
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -107,7 +125,7 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 # App layout
 app.layout = html.Div(
     [
-        dcc.Graph(id="sun-plot", style={"height": 1000}),
+        dcc.Graph(id="sun-plot", style={"height": 1234}),
         dcc.Dropdown(id="id-select", options=all_ids_options, value=""),
     ]
 )
@@ -121,7 +139,7 @@ def update_figure(selected_id: Union[str, int]):
     Args:
         selected_id (Union[str, int]): the id of the selected material
     """
-    if selected_id == "":
+    if selected_id == "" or selected_id is None:
         lvl = None
     else:
         lvl = level_data[selected_id]
@@ -132,9 +150,11 @@ def update_figure(selected_id: Union[str, int]):
             labels=sb_labs,
             parents=sb_parents,
             values=sb_values,
+            insidetextfont=dict(size=30),
             level=lvl,
         )
     )
+    fig.update_layout(uniformtext=dict(minsize=22))
 
     return fig
 
